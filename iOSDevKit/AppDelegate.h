3d03f15d6c8b8cf841472d7bfb12ed66f12ef7d5
jetter <jetter@fy.com>
2020-02-21 20:42:56 +0800

    Initial Commit

---
+++ iOSDevKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSDevKit
+//
+//  Created by fangyuan on 2020/2/21.
+//  Copyright © 2020 fangyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOSDevKit/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  iOSDevKit
+//
+//  Created by fangyuan on 2020/2/21.
+//  Copyright © 2020 fangyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
